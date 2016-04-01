d1b79116090c35f5b79be44438dcfc535ba20864
Ahmed Badawy <admin@ahmed-badawy.com>
2016-04-01 20:47:01 +0200

    updating

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+
+
+class App extends React.Component{
+	render(){
+		return <h3>React Says Welcome</h3>
+	}
+}
+
+// ReactDOM.render(
+// 	<App text="this is the prompt text" />
+// 	document.getElementById("app")
+// )
+
+
+export default App
+
+
+
+
